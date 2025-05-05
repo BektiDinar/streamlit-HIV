e33017c2cf46f03311193c7dea9dbad8696633a5
BektiDinar <1221130031@global.ac.id>
2025-05-05 17:21:01 +0800

    Update stream-hiv.py

--- stream-hiv.py
+++ stream-hiv.py
@@ -7,23 +7,32 @@ hiv_model = pickle.load(open('HIV_model.sav', 'rb'))
 # Judul Web
 st.title('Data Mining Clustering HIV')
 
+# Membagi kolom
+col1, col2 = st.columns(2)
+
 # Indentifikasi inputan 
-Umur = st.text_input('Input Nilai Umur')
+with col1 :
+    Umur = st.text_input('Input Nilai Umur')
 
-Jenis_Kelamin = st.text_input('Input Nilai Jenis Kelamin')
+with col2 :
+    Jenis_Kelamin = st.text_input('Input Nilai Jenis Kelamin')
 
-Kecamatan = st.text_input('Input Nilai Jenis Kecamatan')
+with col1 :
+    Kecamatan = st.text_input('Input Nilai Jenis Kecamatan')
 
-Kelurahan = st.text_input('Input Nilai Jenis Kelurahan')
+with col2 :
+    Kelurahan = st.text_input('Input Nilai Jenis Kelurahan')
 
-Tahun_Register = st.text_input('Input Nilai Jenis Tahun Register')
+with col1 :
+    Tahun_Register = st.text_input('Input Nilai Jenis Tahun Register')
 
 # Code untuk clustering
 hiv_diagnosis = ''
 
 # Membuat tombol untuk clustering
 if st.button('Test Clustering HIV'):
-    hiv_clustering = hiv_model([[Umur, Kelurahan]])
+    hiv_clustering = hiv_model.cluster([[Umur, Jenis_Kelamin, Kecamatan, 
+                                         Kelurahan, Tahun_Register]])
 
     if hiv_clustering[0] == 0:
         hiv_diagnosis = 'Status ODHIV Gagal Follow Up'

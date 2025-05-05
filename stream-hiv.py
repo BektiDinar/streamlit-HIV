619dc87105e72d32dee331faea943bc0e031ddaf
BektiDinar <1221130031@global.ac.id>
2025-05-05 17:05:53 +0800

    Update stream-hiv.py

--- stream-hiv.py
+++ stream-hiv.py
@@ -21,15 +21,9 @@ Tahun_Register = st.text_input('Input Nilai Jenis Tahun Register')
 # Code untuk clustering
 hiv_diagnosis = ''
 
-import pandas as pd
-# Assuming your data is in a Pandas DataFrame called 'data'
-features = df[['Umur', 'Jenis_Kelamin', 'Kecamatan']].values
-hiv_clustering = hiv_model.cluster(features)
-
 # Membuat tombol untuk clustering
 if st.button('Test Clustering HIV'):
-    hiv_clustering = hiv_model.cluster([[Umur, Jenis_Kelamin, Kecamatan, 
-                                         Kelurahan, Tahun_Register]])
+    hiv_clustering = hiv_model.cluster([[Umur, Kelurahan]])
 
     if hiv_clustering[0] == 0:
         hiv_diagnosis = 'Status ODHIV Gagal Follow Up'

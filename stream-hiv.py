fb48a3e5ff3d54ff797e518c001c9d7c057f2922
BektiDinar <1221130031@global.ac.id>
2025-05-05 16:33:52 +0800

    Update stream-hiv.py

--- stream-hiv.py
+++ stream-hiv.py
@@ -2,7 +2,42 @@ import pickle
 import streamlit as st
 
 # Membaca model
-hiv_model = pickle.load(open(HIV_model.sav))
+HIV_model = pickle.load(open('HIV_model.sav', 'rb'))
 
 # Judul Web
 st.title('Data Mining Clustering HIV')
+
+# Membagi kolom
+col1, col2 = st.columns(2)
+
+# Indentifikasi inputan 
+with col1 :
+    Umur = st.text_input('Input Nilai Umur')
+
+with col2 :
+    Jenis Kelamin = st.text_input('Input Nilai Jenis Kelamin')
+
+with col1 :
+    Kecamatan = st.text_input('Input Nilai Jenis Kecamatan')
+
+with col2 :
+    Kelurahan = st.text_input('Input Nilai Jenis Kelurahan')
+
+with col1 :
+    Tahun Register = st.text_input('Input Nilai Jenis Tahun Register')
+
+# Code untuk clustering
+hiv_diagnosis = ''
+
+# Membuat tombol untuk clustering
+if st.button('Test Clustering HIV'):
+    hiv_clustering = HIV_model.cluster([[Umur, Jenis Kelamin, Kecamatan, Kelurahan, Tahun Register]])
+
+if(hiv_clustering[0] == 0):
+    hiv_diagnosis = 'Status ODHIV Gagal Follow Up'
+elif(hiv_clustering[0] == 1):
+    hiv_diagnosis = 'Status ODHIV Meninggal'
+else :
+    hiv_diagnosis = 'Status ODHIV Sedang Pengobatan'
+
+st.success(hiv_diagnosis)
